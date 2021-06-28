3d66f6337b5b532b8fbc81bb3349efa19e12b227
John <vojohn95@gmail.com>
2021-06-27 19:22:01 -0500

    Submenu finished

--- app/Http/Livewire/Navigation.php
+++ app/Http/Livewire/Navigation.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class Navigation extends Component
 {
     public function render()
     {
-        return view('livewire.navigation');
+        $categories = Category::get();
+
+        return view('livewire.navigation', compact('categories'));
     }
 }
