51b8e2d1729971fd7b48afb1005f03e47a4fee13
John <vojohn95@gmail.com>
2021-09-08 20:07:57 -0500

    optimize

--- app/Http/Livewire/UpdateCartItem.php
+++ app/Http/Livewire/UpdateCartItem.php
@@ -4,10 +4,38 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+
 class UpdateCartItem extends Component
 {
 
-    public $rowId;
+    public $rowId, $qty, $quantity;
+
+    public function mount()
+    {
+        $item = Cart::get($this->rowId);
+        $this->qty = $item->qty;
+
+        $this->quantity = qty_available($item->id);
+    }
+
+    public function decrement()
+    {
+        $this->qty = $this->qty - 1;
+
+        Cart::update($this->rowId, $this->qty);
+
+        $this->emit('render');
+    }
+
+    public function increment()
+    {
+        $this->qty = $this->qty + 1;
+
+        Cart::update($this->rowId, $this->qty);
+
+        $this->emit('render');
+    }
 
     public function render()
     {
--- resources/views/livewire/update-cart-item.blade.php
+++ resources/views/livewire/update-cart-item.blade.php
@@ -1,9 +1,14 @@
-<div class="flex item-center">
-    <x-jet-secondary-button>
+<div class="flex items-center" x-data>
+    <x-jet-secondary-button disabled x-bind:disabled="$wire.qty <= 1" wire:loading.attr="disabled"
+        wire:target="decrement" wire:click="decrement">
         -
     </x-jet-secondary-button>
-    <span class="mx-2">1</span>
-    <x-jet-secondary-button>
+
+    <span class="mx-2 text-gray-700">{{ $qty }}</span>
+
+    <x-jet-secondary-button x-bind:disabled="$wire.qty >= $wire.quantity" wire:loading.attr="disabled"
+        wire:target="increment" wire:click="increment">
         +
     </x-jet-secondary-button>
+
 </div>
