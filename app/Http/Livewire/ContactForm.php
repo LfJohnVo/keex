44aeb11ecdb4cb8cd76a899e5905418994741460
vojohn95 <vojohn95@gmail.com>
2022-03-03 03:24:55 -0600

    contact form

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Livewire;
 
+use App\Mail\ContactEmail;
 use Livewire\Component;
+use Illuminate\Support\Facades\Mail;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class ContactForm extends Component
 {
@@ -29,12 +32,28 @@ class ContactForm extends Component
         $rules = $this->rules;
         $this->validate($rules);
 
+        $to = 'vojohn95@gmail.com'; // aqui coloca el email de quien recibira el correo
+        $from_email = 'test@hostinger-tutorials.com';
+        $subject = 'Correo recibido desde la web keex';
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+        $headers .= "From: $from_email" . "\r\n" .
+            "Reply-To: $from_email" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+        $message = '<body><h1>KEEX - Mensaje</h1><p>Este es un <strong> mensaje recibido desde la web KEEX.</strong>.</p>
+        <p>Nombre: ' . $this->nombre . '</p>
+        <p>Email: ' . $this->email . '</p>
+        <p>Mensaje: ' . $this->mensaje . '</p>
+        <p></a></p></body>';
+        mail($to, $subject, $message, $headers);
+
         $this->alert('success', 'Mensaje enviado', [
             'position' => 'top-end',
             'timer' => '4000',
             'toast' => true,
             'timerProgressBar' => true,
-           ]);
+        ]);
+
 
         $this->reset();
     }
